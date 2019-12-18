be7d61409243aa8de259c3e2dcd8ccd1f934b009
M. Nahin Khan <nahin@Nahins-MacBook-Pro.local>
2019-12-18 11:51:53 +0300

    december back at it

--- sumOverAllAnalysis.py
+++ sumOverAllAnalysis.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 RBP_to_show = "AUF1-UNFILTERED"
-isWeighted = False
+isWeighted = True
 
 fig, axes = plt.subplots(1, 2)
 
@@ -9,7 +9,7 @@ fig, axes = plt.subplots(1, 2)
 for rna,ax in zip(["Neat1", "Malat1"],axes):
 
 	#plt.figure()
-	division_factor = 50 if rna=="Malat1" else 1.5
+	#division_factor = 50 if rna=="Malat1" else 1.5
 
 
 
@@ -26,14 +26,16 @@ for rna,ax in zip(["Neat1", "Malat1"],axes):
 
 	for i in x:
 		if AUF1_sites.isOverlap((i,i)):
-			y[i] = int(AUF1_sites.nearestSite((i,i))[0][2].split()[1])/division_factor 
+			y[i] = int(AUF1_sites.nearestSite((i,i))[0][2].split()[1]) 
 		else:
 			y[i] = 0
 
 	ax.plot(depth_array,label = "Depth of RBP binding on "+rna)
-	ax.plot(y,'r',label="AUF1 binding sites")
+	ax2 = ax.twinx()
+	ax2.plot(y,'r',label="AUF1 binding sites")
 
 	ax.legend(loc='upper left')
+	ax2.legend(loc='upper right')
 
 
 
@@ -45,8 +47,9 @@ for rna,ax in zip(["Neat1", "Malat1"],axes):
 	k=0
 	for start,end,annotation in AUF1_competition_range:
 		competitive_score = max(depth_array[start:end+1])
-		plt.annotate(competitive_score,((start+end)/2,30 + (1 if k%2==0 else -1)*(k)*(0.5)))
-		weightFactor = y[start] if isWeighted else 1
+		ax2.annotate(competitive_score,((start+end)/2,y[start+15]),weight='bold') #20 + (k%4 if k%8>4 else 4-k%4)*(0.5))
+		weightFactor = (y[start+15]) if isWeighted else 1
+		#print(weightFactor)
 		total_sum_score+= weightFactor
 		weighted_sum += weightFactor * competitive_score
 		k+=1
@@ -55,9 +58,10 @@ for rna,ax in zip(["Neat1", "Malat1"],axes):
 
 	print(weighted_average)
 
-	plt.xlabel(rna+" gene base number")
-	plt.ylabel("Number of RBPs that cover " + rna + " on the nucleotide")
-	plt.title("A comparison of AUF1 binding sites and how other RBPs bind the lncRNA "+rna)
+	ax.set_xlabel(rna+" gene base number")
+	ax.set_ylabel("Number of RBPs that cover " + rna + " on the nucleotide")
+	ax2.set_ylabel("AUF1 PARCLIP read (strength of binding site)")
+	ax.set_title("A comparison of AUF1 binding sites and how other RBPs bind the lncRNA "+rna)
 
 
 
